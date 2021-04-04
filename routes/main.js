41a5156be2629041f2592b5e7a00b2c470905f42
yasirarism <55983182+yasirarism@users.noreply.github.com>
2021-04-04 21:03:03 +0700

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -24,10 +24,10 @@ router.get('/config', (req, res) => {
         status: true,
         result: {
             prefix : '/',
-            namabot: 'ZhirrrBot',
-            namaowner: 'Zhirrr',
-            instagram: 'zhirr_ajalah',
-            youtube : 'Gak Punya'
+            namabot: 'MissKatyRoBot',
+            namaowner: 'Yasir Aris M',
+            instagram: 'YasirArisM',
+            youtube : 'Ada'
         }
     }
     res.json(config)
